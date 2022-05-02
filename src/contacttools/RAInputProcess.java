6698a13a1e2654a8da61054034d970c8685d6371
Vasily Demin <vasily.demin@gmail.com>
2022-05-03 00:50:25 +0300

    Initial commit

--- src/contacttools/RAInputProcess.java
+++ src/contacttools/RAInputProcess.java
@@ -1,87 +1,52 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package contacttools;
 
 import java.util.ArrayList;
 
-/**
- *
- * @author vdemin
- */
 public class RAInputProcess {
-    
+
     public static void main(String[] args) {
-        
+
         // Initialize Global Application Settings and read them from CNF file
         AppGlobalSettings globalSettings = new AppGlobalSettings();
         globalSettings.ReadFromCNFfile();
-        
-        // Read input registration and attendance data from Input1 file
-        RegistrationAttendance[] raArray = 
-                RegistrationAttendance.readRAArrayFromCSVFile(
-                        AppGlobalSettings.workingDirectory + 
-                        AppGlobalSettings.inputFile1);
-        
-/*        
-        System.out.println("Results of importing Input1 file:");
-        for (ContactItem iArray : raArray) {
-            iArray.println();
-        }
-*/
-        
-        // Read existing partner contact data from MySQL database
-        ContactItem[] ciArray = ContactItem.readCIArrayFromDB(globalSettings); 
 
-/*
-        System.out.println("Results of importing Contacts data from MySQL:");
-        for (ContactItem iArray : ciArray) {
-            iArray.println();
-        }
-*/        
+        // Read input registration and attendance data from Input1 file
+        RegistrationAttendance[] raArray =
+                RegistrationAttendance.readRAArrayFromCSVFile(
+                        AppGlobalSettings.workingDirectory +
+                                AppGlobalSettings.inputFile1);
+
+        // Read existing partner contact data from MySQL database
+        ContactItem[] ciArray = ContactItem.readCIArrayFromDB(globalSettings);
+
         // Read existing event list from MySQL database
         EventItem[] eventArray = EventItem.ReadEventListFromDB();
-        
-/*
-        System.out.println("Results of importing Events data from MySQL:");
-        for(EventItem iArray : eventArray)
-            iArray.println();
-*/
+
         // Read existing registration and attendance data from MySQL database
         RegAttDB[] raDB = RegAttDB.readRADBfromMySQL(globalSettings);
-        
-/*
-        System.out.println("Results of importing registration and attendance data from MySQL:");
-        if(raDB != null)
-            for(RegAttDB iArray : raDB)
-                iArray.println();
-        else
-            System.out.println("raDB is empty!");
-*/
-        // Define free index set for ciArray: nextIDAfreMax & availableIDsArray[]
+
+        // Define free index set for ciArray: nextIDAfterMax & availableIDsArray[]
         int nextIDAfterMax = 0;
         int[] availableIDsArray = null;
-        
-        if(ciArray != null){
-            int n = ciArray.length;
-        
-            for(int i = 0; i < n; i++)
-                if(ciArray[i].getId() > nextIDAfterMax){
-                    nextIDAfterMax = ciArray[i].getId();
+
+        if(ciArray.length > 0){
+
+            for (ContactItem contactItem : ciArray)
+                if (contactItem.getId() > nextIDAfterMax) {
+                    nextIDAfterMax = contactItem.getId();
                 }
             nextIDAfterMax++;
-            
+
             if( (nextIDAfterMax-1) > ciArray.length){
                 availableIDsArray = new int[nextIDAfterMax - ciArray.length - 1];
                 int iID = 0;
-                
+
                 for(int j = 1; j < nextIDAfterMax; j++){
                     boolean jIDExist = false;
                     for (ContactItem ciArray1 : ciArray) {
                         if (ciArray1.getId() == j) {
                             jIDExist = true;
+                            break;
                         }
                     }
                     if(!jIDExist) {
@@ -89,31 +54,30 @@ public class RAInputProcess {
                         iID++;
                     }
                 }
-                
+
             }
-            
-        } else 
+
+        } else
             nextIDAfterMax = 1;
-        
+
         // Define free index set for raDB: nextRAAfterMax and availableRAIndexArray[]
         // Set ID index field for raArray
         int nextRAAfterMax = 0;
         int[] availableRAIndexArray = null;
-        
-        if(raDB != null){
-            int n = raDB.length;
-        
-            for(int i = 0; i < n; i++)
-                if(raDB[i].getValueByIndex(0) > nextRAAfterMax){
-                    nextRAAfterMax = raDB[i].getValueByIndex(0);
+
+        if(raDB.length > 0){
+
+            for (RegAttDB regAttDB : raDB)
+                if (regAttDB.getValueByIndex(0) > nextRAAfterMax) {
+                    nextRAAfterMax = regAttDB.getValueByIndex(0);
                 }
             nextRAAfterMax++;
-            
+
             if( (nextRAAfterMax-1) > raDB.length){
                 availableRAIndexArray = new int[nextRAAfterMax - raDB.length - 1];
-                
+
                 int iID = 0;
-                
+
                 for(int j = 1; j < nextRAAfterMax; j++){
                     boolean jRAExist = false;
                     for (RegAttDB raDB1 : raDB) {
@@ -127,18 +91,18 @@ public class RAInputProcess {
                     }
                 }
             }
-            
-        } else 
+
+        } else
             nextRAAfterMax = 1;
-        
+
         //Find new contacts in raArray, copy them to separate ciInputArray
-        
+
         ContactItem[] ciInputArray = null;
         ArrayList<ContactItem> ciInArr = new ArrayList<>();
-        
+
         ArrayList<Integer> newCIindexes = new ArrayList<>();
-        
-        if(ciArray != null){
+
+        if(ciArray.length > 0){
             for(int i = 0; i < raArray.length; i++) {
                 String s1 = raArray[i].getValueByHeader("email");
                 s1 = s1.toLowerCase();
@@ -156,17 +120,17 @@ public class RAInputProcess {
                     newCIindexes.add(i);
                     RegistrationAttendance.copyCIfromRA(ciItem, raArray[i]);
                     ciInArr.add(ciItem);
-                }        
+                }
             }
         }
-        
+
         if(ciInArr.size() > 0){
             ciInputArray = new ContactItem[ciInArr.size()];
             ciInArr.toArray(ciInputArray);
         }
 
         // Assign IDs to new contacts
-                
+
         if(ciInputArray != null) {
             if(availableIDsArray != null) {
 
@@ -199,75 +163,52 @@ public class RAInputProcess {
 
             }
         }
-        
+
         if(ciInputArray != null) {
             int count = 0;
             for(int i = 0; i < ciInputArray.length; i++)
                 count += raArray[newCIindexes.get(i)].getAttendance();
             System.out.println("New partner's employees participated: " + count);
             System.out.println("New partner's employees registered: " + ciInputArray.length);
-        } else 
+        } else
             System.out.println("New partner's employees registered & participated: 0");
-        
-/*        
-        System.out.println("Results of assigning IDs to raArray:");
-        for (ContactItem iArray : raArray) {
-            iArray.println();
-        }
-*/        
 
-/*
-        System.out.println("New contact items from raArray:");
-        for (ContactItem iArray : ciInputArray) {
-            iArray.println();
-        }
-*/        
         // Write new contact items to MySQL database
         ContactItem.writeCIArrayToDB(ciInputArray, globalSettings);
-        
-/*
-        // Write new contact items to file outputFile1
-        if(ciInputArray != null) {
-            ContactItem.exportCIArrayToCSVfile(AppGlobalSettings.workingDirectory +
-                    AppGlobalSettings.outputFile1, ciInputArray);
-        } else {
-            System.out.println("There is no any new contacts in the input file, nothing to export!");
-        }
- */
 
         // Add existing and new contacts to ciNewArray
-        ContactItem[] ciNewArray; 
-        
+        ContactItem[] ciNewArray;
+
         if(ciInputArray != null) {
             ciNewArray = new ContactItem[ciArray.length + ciInputArray.length];
             System.arraycopy(ciArray, 0, ciNewArray, 0, ciArray.length);
             System.arraycopy(ciInputArray, 0, ciNewArray, ciArray.length, ciInputArray.length);
             ContactItem.exportCIArrayToCSVfile(AppGlobalSettings.workingDirectory +
-                AppGlobalSettings.outputFile, ciNewArray);
+                    AppGlobalSettings.outputFile, ciNewArray);
         } else {
             ciNewArray = ciArray;
         }
-        
+
         // Export existing and new (all) contacts to csv file
         ContactItem.exportCIArrayToCSVfile(AppGlobalSettings.workingDirectory +
                 AppGlobalSettings.outputFile, ciNewArray);
-        
-        // Analize new registration and attendance data, add new records to the raInputArray
+
+        // Analyze new registration and attendance data, add new records to the raInputArray
         // add changed records to the raUpdateArray
         ArrayList<RegistrationAttendance> raInArr = new ArrayList<>();
         ArrayList<RegistrationAttendance> raUpdArr = new ArrayList<>();
         ArrayList<Integer> newRAindexes = new ArrayList<>();
-                
+
         for(int i = 0; i < raArray.length; i++) {
             if(raArray[i].getEventID() == 0) continue;
             boolean raFound = false;
-            for(int j = 0; j < raDB.length; j++) {
-                if((raArray[i].getEventID() == raDB[j].getValueByHeader("fk_event_id")) & (raArray[i].getId() == raDB[j].getValueByHeader("fk_id"))) {
+            for (RegAttDB regAttDB : raDB) {
+                if ((raArray[i].getEventID() == regAttDB.getValueByHeader("fk_event_id")) & (raArray[i].getId() == regAttDB.getValueByHeader("fk_id"))) {
                     raFound = true;
-                    raArray[i].setRegID(raDB[j].getValueByHeader("attendance_id"));
-                    if(raArray[i].getRegistration() != 0) {
-                        if(raArray[i].getAttendance() != raDB[j].getValueByHeader("attendance_status")){
-                            raArray[i].setAttendance(raDB[j].getValueByHeader("attendance_status"));
+                    raArray[i].setRegID(regAttDB.getValueByHeader("attendance_id"));
+                    if (raArray[i].getRegistration() != 0) {
+                        if (raArray[i].getAttendance() != regAttDB.getValueByHeader("attendance_status")) {
+                            raArray[i].setAttendance(regAttDB.getValueByHeader("attendance_status"));
                             raUpdArr.add(raArray[i]);
                         }
                     } else {
@@ -283,19 +224,19 @@ public class RAInputProcess {
                 raInArr.add(raArray[i]);
             }
         }
-        
+
         RegistrationAttendance[] raInputArray = null;
         if(raInArr.size() != 0) {
             raInputArray = new RegistrationAttendance[raInArr.size()];
             raInArr.toArray(raInputArray);
         }
-        
+
         RegistrationAttendance[] raUpdateArray = null;
         if(raUpdArr.size() != 0) {
             raUpdateArray = new RegistrationAttendance[raUpdArr.size()];
             raUpdArr.toArray(raUpdateArray);
         }
-        
+
         // Assign Attendance IDs to new Registration Attendance records
         if(raInputArray != null) {
             if(availableRAIndexArray != null) {
@@ -328,7 +269,7 @@ public class RAInputProcess {
                 }
             }
         }
-        
+
         // Write registration and attendance data to MySQL database
         if(raInputArray != null){
             RegAttDB[] raInDB = new RegAttDB[raInputArray.length];
@@ -344,7 +285,7 @@ public class RAInputProcess {
         } else {
             System.out.println("There aren't new registration and attendance data in the input file. Nothing to do");
         }
-        
+
         // Update registration and attendance data to MySQL database
         if(raUpdateArray != null){
             RegAttDB[] raUpdDB = new RegAttDB[raUpdateArray.length];
@@ -360,12 +301,12 @@ public class RAInputProcess {
         } else {
             System.out.println("There aren't updated registration and attendance data in the input file. Nothing to do");
         }
-        
+
         // Read final registration and attendance data from MySQL database
         raDB = RegAttDB.readRADBfromMySQL(globalSettings);
-        
+
         // Export new contacts registration and attendance information into CSV file
-        
+
         if(ciInputArray != null) {
             // Create RegistrationAttendance array with new contacts for export
             RegistrationAttendance[] raNewArray = new RegistrationAttendance[ciInputArray.length];
@@ -378,65 +319,64 @@ public class RAInputProcess {
                     }
                 }
                 if(regAttIndex == -1) {
-                   System.out.println("Warning: no registration & attendance info connected with this contact");
-                   ciInputArray[i].println();
-                   raNewArray[i] = new RegistrationAttendance(0, 0, ciInputArray[i], 0, 0);
-                   continue;
+                    System.out.println("Warning: no registration & attendance info connected with this contact");
+                    ciInputArray[i].println();
+                    raNewArray[i] = new RegistrationAttendance(0, 0, ciInputArray[i], 0, 0);
                 } else
-                    raNewArray[i] = new RegistrationAttendance(raDB[regAttIndex].getValueByIndex(0), raDB[regAttIndex].getValueByIndex(1), 
-                        ciInputArray[i], raDB[regAttIndex].getValueByIndex(3), raDB[regAttIndex].getValueByIndex(4));
+                    raNewArray[i] = new RegistrationAttendance(raDB[regAttIndex].getValueByIndex(0), raDB[regAttIndex].getValueByIndex(1),
+                            ciInputArray[i], raDB[regAttIndex].getValueByIndex(3), raDB[regAttIndex].getValueByIndex(4));
             }
-            
+
             // Export RegistrationAttendance array to the CSV file
             RegistrationAttendance.exportRAArrayToCSVfile(AppGlobalSettings.workingDirectory +
                     AppGlobalSettings.outputFile1, raNewArray);
-            
+
         } else {
             System.out.println("Error: ciInputArray is empty, no any new contacts for export, nothing to do");
         }
-        
+
         // To build final report table from EventList, ciNewArray, ciInputArray and raDB
-        
+
         int[] PartnersRegistered = new int[eventArray.length];
         int[] PartnersParticipated = new int[eventArray.length];
-        
+
         for(int i = 0; i < eventArray.length; i++){
             PartnersRegistered[i] = 0;
             PartnersParticipated[i] = 0;
             int eventID = eventArray[i].getEventID();
-            for(int j = 0; j < raDB.length; j++) {
-                if(raDB[j].getValueByHeader("fk_event_id") == eventID){
-                    PartnersRegistered[i] += raDB[j].getValueByHeader("registration_status");
-                    PartnersParticipated[i] += raDB[j].getValueByHeader("attendance_status");
+            for (RegAttDB regAttDB : raDB) {
+                if (regAttDB.getValueByHeader("fk_event_id") == eventID) {
+                    PartnersRegistered[i] += regAttDB.getValueByHeader("registration_status");
+                    PartnersParticipated[i] += regAttDB.getValueByHeader("attendance_status");
                 }
             }
             System.out.println("EventID:\t" + eventID + "\tPartners Registered:\t" +
                     PartnersRegistered[i] + "\tPartners Attended:\t" + PartnersParticipated[i]);
         }
-        
+
         // Calculate number of unique attendees
-        
+
         int[] PartnersAttendeeList = new int[ciNewArray.length];
-        for(int j = 0; j < raDB.length; j++)
-            if(raDB[j].getValueByHeader("attendance_status") == 1) {
-                int ciID = raDB[j].getValueByHeader("fk_id");
-                for(int i = 0; i < ciNewArray.length; i++){
-                    if(ciNewArray[i].getId() == ciID) {
+        for (RegAttDB regAttDB : raDB)
+            if (regAttDB.getValueByHeader("attendance_status") == 1) {
+                int ciID = regAttDB.getValueByHeader("fk_id");
+                for (int i = 0; i < ciNewArray.length; i++) {
+                    if (ciNewArray[i].getId() == ciID) {
                         PartnersAttendeeList[i]++;
                         break;
                     }
                 }
             }
-        
+
         int AttendeeCounter = 0;
-        for(int i = 0; i < PartnersAttendeeList.length; i++)
-            if(PartnersAttendeeList[i] > 0)
+        for (int k : PartnersAttendeeList)
+            if (k > 0)
                 AttendeeCounter++;
-        
+
         System.out.println("Total number of unique attendees: " + AttendeeCounter);
-        
+
         // Looking for partners who visited maximum number of events
-        
+
         int maxEventsVisited = 0;
         int lastIndex = -1;
         for(int i = 0; i < PartnersAttendeeList.length; i++)
@@ -444,15 +384,15 @@ public class RAInputProcess {
                 maxEventsVisited = PartnersAttendeeList[i];
                 lastIndex = i;
             }
-        
+
         int maxEventsVisitedCounter = 0;
-        for(int i = 0; i < PartnersAttendeeList.length; i++)
-            if(PartnersAttendeeList[i] == maxEventsVisited)
+        for (int j : PartnersAttendeeList)
+            if (j == maxEventsVisited)
                 maxEventsVisitedCounter++;
-        
+
         System.out.println("Maximum number of events, visited by single person: " + maxEventsVisited);
         System.out.println("Number of persons with maximum visited events: " + maxEventsVisitedCounter);
-        System.out.println("Last Index = " + lastIndex); 
+        System.out.println("Last Index = " + lastIndex);
         ciNewArray[lastIndex].println();
     }
 }
